c8b307c1ed0ce1098ba4112844b081c4299d75a1
Thomas Gorissen <thomas.gorissen@me.com>
2025-06-15 22:32:05 +0800

    Support for IMMEDIATELY

--- src/languages/l4.js
+++ src/languages/l4.js
@@ -8,7 +8,7 @@ export default function(hljs) {
   const KEYWORDS = {
     keyword: [
       'GIVEN', 'GIVETH', 'DECIDE', 'MEANS', 'DECLARE', 'IF', 'THEN', 'ELSE', 'OTHERWISE',
-      'AND', 'OR', 'RAND', 'ROR', 'IS', 'HAS', 'ONE', 'OF', 'WITH', 'A', 'AN', 'THE',
+      'AND', 'OR', 'RAND', 'ROR', 'IS', 'IMMEDIATELY', 'HAS', 'ONE', 'OF', 'WITH', 'A', 'AN', 'THE',
       'YIELD', 'CONSIDER', 'WHERE', 'LIST', 'ASSUME', 'WHEN', 'TYPE', 'PARTY', 'DO', 'DOES',
       'MUST', 'WITHIN', 'HENCE', 'LEST', 'FUNCTION', 'FROM', 'TO', 'IMPLIES',
       'AT', 'STARTING', 'FOLLOWED', 'BY', 'FOR', 'ALL', 'PROVIDED', 'EXACTLY',
